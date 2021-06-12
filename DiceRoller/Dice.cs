02fdade803a3d751ca40c4bcca7e9d07d7ac4427
sebastien Bancquart <sebastienbancquart@gmail.com>
2021-06-12 22:46:07 +0200

    Ajoutez des fichiers projet.

---
+++ DiceRoller/Dice.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace DiceRoller
+{
+    internal static class Dice
+    {
+        private static readonly Random Rand = new();
+
+        internal static int Parse(string entry)
+        {
+            entry = entry.Trim().ToUpper();
+            string[] RollDefinitions = entry.Split(new char[] { '+' });
+            return RollDefinitions.Sum(p => Roll(p));
+        }
+
+        private static int Roll(string rollDefinition)
+        {
+            rollDefinition = rollDefinition.Trim();
+            if (rollDefinition.Contains('D'))
+            {
+                string[] DiceDefinition = rollDefinition.Split(new char[] { 'D' });
+                int NumberOfDices = int.Parse(DiceDefinition[0]);
+                int NumberOfFaces = int.Parse(DiceDefinition[1]);
+                int Result = 0;
+                for (int i = 0;i<NumberOfDices;i++)
+                {
+                    Result += RollDice(NumberOfFaces);
+                }
+                return Result;
+            }
+            else
+            {
+                return int.Parse(rollDefinition);
+            }
+        }
+
+        private static int RollDice(int numberOfFaces)
+        {
+            return Rand.Next(numberOfFaces) + 1;
+        }
+    }
+}
---
+++ DiceRoller/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DiceRoller
+{
+    internal static class Program
+    {
+        private static void Main(string[] _)
+        {
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Out.Write("-->");
+                string Entry = Console.In.ReadLine();
+                if (Entry.Length > 0)
+                {
+                    int Result = Dice.Parse(Entry);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Out.Write(Result.ToString() + Environment.NewLine + Environment.NewLine);
+                }
+            }
+        }
+    }
+}
